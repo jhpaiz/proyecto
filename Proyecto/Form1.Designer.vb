f9b7d932c18909ba742a71827adb1b48579a31bc
jhpaiz <metalzocial@gmail.com>
2020-11-15 22:50:09 -0600

    inicializacion del repositorio

---
+++ Proyecto/Form1.Designer.vb
@@ -0,0 +1,396 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.Label20 = New System.Windows.Forms.Label()
+        Me.ListBox5 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(78, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(507, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ESTADISTICA EVALUATIVA COMPUTACIONAL"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(139, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(332, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ingrese el total de elementos de la muestra a ingresar y de clic en OK"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(191, 101)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(322, 101)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(142, 149)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(167, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Ingrese elemento y presione Enter"
+        Me.Label3.Visible = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(322, 142)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(75, 20)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.Visible = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(83, 202)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(36, 316)
+        Me.ListBox1.TabIndex = 7
+        Me.ListBox1.Visible = False
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(142, 202)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(47, 316)
+        Me.ListBox2.TabIndex = 9
+        Me.ListBox2.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(363, 202)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(63, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Valor Mayor"
+        Me.Label4.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(521, 202)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(64, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Valor Menor"
+        Me.Label5.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(432, 202)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Label6"
+        Me.Label6.Visible = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(591, 202)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Label7"
+        Me.Label7.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(454, 245)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(122, 23)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Mostrar Resultados"
+        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.Visible = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(463, 322)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(36, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Media"
+        Me.Label8.Visible = False
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(451, 365)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(48, 13)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "Mediana"
+        Me.Label9.Visible = False
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(465, 415)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(34, 13)
+        Me.Label10.TabIndex = 17
+        Me.Label10.Text = "Moda"
+        Me.Label10.Visible = False
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(394, 456)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(105, 13)
+        Me.Label11.TabIndex = 18
+        Me.Label11.Text = "Desviación Estándar"
+        Me.Label11.Visible = False
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(523, 321)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(0, 13)
+        Me.Label12.TabIndex = 19
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(523, 365)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(0, 13)
+        Me.Label13.TabIndex = 20
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(523, 415)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(0, 13)
+        Me.Label14.TabIndex = 21
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(523, 456)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(0, 13)
+        Me.Label15.TabIndex = 22
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Location = New System.Drawing.Point(207, 202)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(50, 316)
+        Me.ListBox3.TabIndex = 24
+        Me.ListBox3.Visible = False
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.Location = New System.Drawing.Point(279, 202)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(50, 316)
+        Me.ListBox4.TabIndex = 25
+        Me.ListBox4.Visible = False
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(13, 186)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(39, 13)
+        Me.Label16.TabIndex = 26
+        Me.Label16.Text = "Rango"
+        Me.Label16.Visible = False
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(83, 185)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(10, 13)
+        Me.Label17.TabIndex = 27
+        Me.Label17.Text = "f"
+        Me.Label17.Visible = False
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(142, 185)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(16, 13)
+        Me.Label18.TabIndex = 28
+        Me.Label18.Text = "fa"
+        Me.Label18.Visible = False
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(207, 185)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(14, 13)
+        Me.Label19.TabIndex = 29
+        Me.Label19.Text = "xi"
+        Me.Label19.Visible = False
+        '
+        'Label20
+        '
+        Me.Label20.AutoSize = True
+        Me.Label20.Location = New System.Drawing.Point(279, 185)
+        Me.Label20.Name = "Label20"
+        Me.Label20.Size = New System.Drawing.Size(55, 13)
+        Me.Label20.TabIndex = 30
+        Me.Label20.Text = "f ( xi - x)^2"
+        Me.Label20.Visible = False
+        '
+        'ListBox5
+        '
+        Me.ListBox5.FormattingEnabled = True
+        Me.ListBox5.Location = New System.Drawing.Point(16, 203)
+        Me.ListBox5.Name = "ListBox5"
+        Me.ListBox5.Size = New System.Drawing.Size(50, 316)
+        Me.ListBox5.TabIndex = 31
+        Me.ListBox5.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(646, 530)
+        Me.Controls.Add(Me.ListBox5)
+        Me.Controls.Add(Me.Label20)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.ListBox4)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents ListBox3 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox4 As System.Windows.Forms.ListBox
+    Friend WithEvents Label16 As System.Windows.Forms.Label
+    Friend WithEvents Label17 As System.Windows.Forms.Label
+    Friend WithEvents Label18 As System.Windows.Forms.Label
+    Friend WithEvents Label19 As System.Windows.Forms.Label
+    Friend WithEvents Label20 As System.Windows.Forms.Label
+    Friend WithEvents ListBox5 As System.Windows.Forms.ListBox
+
+End Class
---
+++ Proyecto/Form1.vb
@@ -0,0 +1,224 @@
+﻿Imports System.Math
+Public Class Form1
+    Dim total As Integer
+    Dim muestra(0) As Integer
+    Dim frecuencia(10) As Integer
+    Dim frecacum(10) As Integer
+    Dim marca(10) As Double
+    Dim frecpormarca(11) As Double
+    Dim marcamedia(11) As Double
+    Dim i As Integer = 0
+    Dim media, lri, lri1, mediana, moda, dv As Double
+    Dim fx, f, fai, fai1, fas As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Val(TextBox1.Text) Then
+            total = TextBox1.Text
+            TextBox1.ReadOnly = True
+            ReDim muestra(total)
+            Label3.Visible = True
+            TextBox2.Visible = True
+            ListBox1.Visible = True
+            Button1.Enabled = False
+        Else : MsgBox("Dato incorrecto", MsgBoxStyle.OkOnly)
+            TextBox1.Clear()
+            TextBox1.Focus()
+        End If
+
+    End Sub
+
+    Private Sub TextBox2_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles TextBox2.KeyDown
+        Select Case e.KeyData
+            Case Keys.Enter
+                If Val(TextBox2.Text) Then
+                    If TextBox2.Text >= 1 And TextBox2.Text <= 100 Then
+                        muestra(i) = TextBox2.Text
+                        ListBox1.Items.Add(TextBox2.Text)
+                        i = i + 1
+                    Else : MsgBox("Fuera de rango", MsgBoxStyle.OkOnly)
+                    End If
+                Else : MsgBox("Dato incorrecto", MsgBoxStyle.OkOnly)
+                End If
+                If i = total Then
+                    MsgBox("Ingreso completado, se ordenaran los datos", MsgBoxStyle.OkOnly)
+                    Array.Sort(muestra)
+                    ListBox1.Items.Clear()
+                    For j As Integer = 1 To total Step 1
+                        ListBox1.Items.Add(muestra(j))
+                    Next
+                    MsgBox("Datos ordenados", MsgBoxStyle.OkOnly)
+                    Label3.Visible = False
+                    TextBox2.Visible = False
+                    Label4.Visible = True
+                    Label5.Visible = True
+                    Label6.Text = muestra(total)
+                    Label7.Text = muestra(1)
+                    Label6.Visible = True
+                    Label7.Visible = True
+                    Button3.Visible = True
+                End If
+                TextBox2.Clear()
+                TextBox2.Focus()
+        End Select
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label8.Visible = True
+        Label9.Visible = True
+        Label10.Visible = True
+        Label11.Visible = True
+        Label16.Visible = True
+        Label17.Visible = True
+        Label18.Visible = True
+        Label19.Visible = True
+        Label20.Visible = True
+        ListBox2.Visible = True
+        ListBox3.Visible = True
+        ListBox4.Visible = True
+        ListBox5.Visible = True
+        For x As Integer = 1 To total Step 1
+            Select Case muestra(x)
+                Case 1 To 10
+                    frecuencia(1) = frecuencia(1) + 1
+                Case 11 To 20
+                    frecuencia(2) = frecuencia(2) + 1
+                Case 21 To 30
+                    frecuencia(3) = frecuencia(3) + 1
+                Case 31 To 40
+                    frecuencia(4) = frecuencia(4) + 1
+                Case 41 To 50
+                    frecuencia(5) = frecuencia(5) + 1
+                Case 51 To 60
+                    frecuencia(6) = frecuencia(6) + 1
+                Case 61 To 70
+                    frecuencia(7) = frecuencia(7) + 1
+                Case 71 To 80
+                    frecuencia(8) = frecuencia(8) + 1
+                Case 81 To 90
+                    frecuencia(9) = frecuencia(9) + 1
+                Case 91 To 100
+                    frecuencia(10) = frecuencia(10) + 1
+            End Select
+        Next
+        For y As Integer = 1 To 10 Step 1
+            frecacum(y) = frecuencia(y) + frecacum(y - 1)
+        Next
+        i = 0
+        For z As Integer = 1 To 100 Step 10
+            i = i + 1
+            marca(i) = (z + (z + 9)) / 2
+            ListBox5.Items.Add(z & " - " & z + 9)
+        Next
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+        For k As Integer = 1 To 10 Step 1
+            ListBox1.Items.Add(frecuencia(k))
+            ListBox2.Items.Add(frecacum(k))
+            ListBox3.Items.Add(marca(k))
+            frecpormarca(k) = frecuencia(k) * marca(k)
+            frecpormarca(11) = frecpormarca(11) + frecpormarca(k)
+        Next
+        media = frecpormarca(11) / total
+        For k As Integer = 1 To 10 Step 1
+            marcamedia(k) = frecuencia(k) * ((marca(k) - media) ^ 2)
+            marcamedia(11) = marcamedia(11) + marcamedia(k)
+            ListBox4.Items.Add(marcamedia(k))
+        Next
+        Select Case media
+            Case 1 To 10
+                lri = 0.5
+                fai = frecacum(0)
+                fx = frecuencia(1)
+            Case 11 To 20
+                lri = 10.5
+                fai = frecacum(1)
+                fx = frecuencia(2)
+
+                lri1 = 0.5
+                f = frecuencia(1)
+                fai1 = frecuencia(0)
+                fas = frecuencia(2)
+            Case 21 To 30
+                lri = 20.5
+                fai = frecuencia(2)
+                fx = frecuencia(3)
+
+                lri1 = 10.5
+                f = frecuencia(2)
+                fai1 = frecuencia(1)
+                fas = frecuencia(3)
+            Case 31 To 40
+                lri = 30.5
+                fai = frecacum(3)
+                fx = frecuencia(4)
+
+                lri1 = 20.5
+                f = frecuencia(3)
+                fai1 = frecuencia(2)
+                fas = frecuencia(4)
+            Case 41 To 50
+                lri = 40.5
+                fai = frecacum(4)
+                fx = frecuencia(5)
+
+                lri1 = 30.5
+                f = frecuencia(4)
+                fai1 = frecuencia(3)
+                fas = frecuencia(5)
+            Case 51 To 60
+                lri = 50.5
+                fai = frecacum(5)
+                fx = frecuencia(6)
+
+                lri1 = 40.5
+                f = frecuencia(5)
+                fai1 = frecuencia(4)
+                fas = frecuencia(6)
+            Case 61 To 70
+                lri = 60.5
+                fai = frecacum(6)
+                fx = frecuencia(7)
+
+                lri1 = 50.5
+                f = frecuencia(6)
+                fai1 = frecuencia(5)
+                fas = frecuencia(7)
+            Case 71 To 80
+                lri = 70.5
+                fai = frecacum(7)
+                fx = frecuencia(8)
+
+                lri1 = 60.5
+                f = frecuencia(7)
+                fai1 = frecuencia(6)
+                fas = frecuencia(8)
+
+            Case 81 To 90
+                lri = 80.5
+                fai = frecacum(8)
+                fx = frecuencia(9)
+
+                lri1 = 70.5
+                f = frecuencia(8)
+                fai1 = frecuencia(7)
+                fas = frecuencia(9)
+            Case 91 To 100
+                lri = 90.5
+                fai = frecacum(9)
+                fx = frecuencia(10)
+
+                lri1 = 80.5
+                f = frecuencia(9)
+                fai1 = frecuencia(8)
+                fas = frecuencia(10)
+        End Select
+        mediana = lri + (((total / 2) - fai) / fx) * 10
+        moda = lri1 + (f - fai1) / ((f - fai1) + (f - fas)) * 10
+        dv = Sqrt(marcamedia(11) / total)
+        Label12.Text = media
+        Label13.Text = mediana
+        Label14.Text = moda
+        Label15.Text = dv
+        Button3.Enabled = False
+    End Sub
+End Class
